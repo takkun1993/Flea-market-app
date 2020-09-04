3a547e83e6c0804811bc3e87324a7d0b28fd876b
tsuyoshi-ueda-ri <63494238+tsuyoshi-ueda-ri@users.noreply.github.com>
2020-09-05 07:58:44 +0900

    Revert "マークアップ前準備完了"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'items/item_detail'
-  get 'items/item_exhibition'
-  get 'items/item_purchase'
   root 'items#index'
-  resources :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
