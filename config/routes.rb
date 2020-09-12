be220ce2f5da7d284f389040133ed7c58c65e9a5
kobata2203 <kobata1569@icloud.com>
2020-09-12 12:16:41 +0900

    商品詳細ページの作成完了
    商品詳細ページ・ヘッダー・フッター・商品購入確認ページ・支払い方法ページ・配送先ページの作成が完了しました。

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'items/item_detail'
   get 'items/item_exhibition'
   get 'items/item_purchase'
+  get 'items/top_page_header'
+  get 'items/top_page_footer'
+  get 'items/payment_method'
+  get 'items/shipping_address'
   root 'items#index'
   resources :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
