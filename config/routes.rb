ef8a46bd53116b2ab7cb972a829f289b138529fb
tsuyoshi-ueda-ri <63494238+tsuyoshi-ueda-ri@users.noreply.github.com>
2020-09-15 22:56:30 +0900

    image_tag,link_to,fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'items/item_detail'
   get 'items/item_exhibition'
   get 'items/item_purchase'
-  get 'cards/new'
+  resources :cards, only: :new
   root 'items#index'
   resources :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
