09555fd9af23496b8b29eea88782b8c5e2e651ab
kobata2203 <kobata1569@icloud.com>
2020-09-17 10:54:43 +0900

    コンフリクト箇所修正
    コンフリクト箇所修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   get 'items/item_detail'
   get 'items/item_exhibition'
   get 'items/item_purchase'
+  resources :cards, only: :new
+  root 'items#index'
   get 'items/top_page_header'
   get 'items/top_page_footer'
   get 'items/payment_method'
   get 'items/shipping_address'
-  resources :cards, only: :new
-  root 'items#index'
   resources :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
