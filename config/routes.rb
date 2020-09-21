ba866cad3d7b26d9f88f0754593401e4a1a36d3c
takkun1993 <68512825+takkun1993@users.noreply.github.com>
2020-09-21 20:39:13 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
+=======
+  get 'users/index'
+  get 'users/new'
+  get 'users/new2'
+  get 'users/new3'
+  get 'users/sign_in'
+  get 'items/item_detail'
+  get 'items/item_exhibition'
+  get 'items/item_purchase'
+>>>>>>> Stashed changes
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
