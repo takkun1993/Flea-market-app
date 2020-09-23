053f7f18a679a7c1f204f3dc8ae0bd2b043c6baf
takkun1993 <68512825+takkun1993@users.noreply.github.com>
2020-09-23 19:53:50 +0900

    アクション名の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def index
   end
+  def new_users_info
+  end
+  def new_users_address
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'users/index'
   get 'users/new'
-  get 'users/new2'
-  get 'users/new3'
+  get 'users/new_users-info'
+  get 'users/new_users-address'
   get 'users/sign_in'
   get 'items/item_detail'
   get 'items/item_exhibition'
