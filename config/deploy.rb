9b6ee816d48778797bf6cdc53d8eb3f961fceae7
takkun1993 <68512825+takkun1993@users.noreply.github.com>
2020-10-01 17:53:56 +0900

    デプロイの記述

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.14.1'
 
 # 自身のアプリ名、ユーザー名、リポジトリ名を記述
-set :application, 'Flea'
+set :application, 'Flea-market-app'
 set :repo_url,  'git@github.com:takkun1993/Flea-market-app.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
