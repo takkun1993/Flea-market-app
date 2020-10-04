bfac49d3c5f8e8e13cf200c4fe9c136bf006b934
kobata2203 <kobata1569@icloud.com>
2020-10-05 00:33:10 +0900

    search_create
    search_create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,46 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+  def new
+    @item = Item.new
+    @item_img = @item.item_imgs.build
+  end
+  def create
+    Item.create(item_params)
+  end
+  def destroy
+    item = Item.find_by(id: params[:id])
+    item.destroy
+  end
+  def edit
+    @item = Item.find(params[:id])
+  end
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+  end
+  def show
+    @item = Item.find(params[:id])
+  end
+  def search
+    sort = params[:sort] || "created_at DESC"
+    @keyword = params[:keyword]
+
+    if @keyword.present?
+      @items = []
+       # 分割したキーワードごとに検索
+      @keyword.split(/[[:blank:]]+/).each do |keyword|
+        next if keyword == ""
+        @items += Item.where('name LIKE(?) OR description LIKE(?)', "%#{keyword}%", "%#{keyword}%")
+      end
+      @items.uniq!
+    else
+      @items = Item.order(sort)
+    end
+  end
+  private
+  def item_params
+    params.permit(:name, :introduction, :price, :brand_id, :item_condition, :postage_payer, :prefecuture_type, :item_img_id, :ca_code, :size, :preparation_day, :postagetegory_id, :seller_id, :buyer_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   get 'items/top_page_footer'
   get 'items/payment_method'
   get 'items/shipping_address'
+  # post 'items/edit/:id', to: 'items#update'
+  patch 'show_item_path', to: 'items#update'
+  get "search" => "items#search"
   resources :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
