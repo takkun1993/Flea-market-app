b53c167c592882b9b653c3fdeb7770bdedd050ba
kobata2203 <kobata1569@icloud.com>
2020-10-14 07:20:54 +0900

    検索機能update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
   end
   def show
     @items = @category.set_items
-    @items = @items.where(buyer_id: nil).order("created_at DESC").page(params[:page]).per(9)
+    # @items = @items.where(buyer_id: nil).order("created_at DESC").page(params[:page]).per(9)
   end
   
   private
