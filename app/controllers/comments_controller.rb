942a606cd0e9c577a8b71526b8e7217ce7342521
kobata2203 <kobata1569@icloud.com>
2020-10-30 03:30:35 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,19 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = @item.comments.new(comments_params)
-    redirect_to "/items/#{comment.comment.id}"
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit( :comment, :user_id, :item_id)
+    # params.require(:item).permit(:name, :introduction, :price, :prefecture_code, :brand_id, :pref_id, :size_id, :item_condition_id, :postage_payer_id, :preparation_day_id, :postage_type_id, :category_id, :trading_status, item_imgs_attributes: [:url, :id]).merge(seller_id: current_user.id)
   end
 end
