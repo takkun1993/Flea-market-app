1bdd27fd547e9f9a25ed3e843a6f0c97035ae3ca
kobata2203 <kobata1569@icloud.com>
2020-10-30 12:23:53 +0900

    商品詳細表示・コメント機能update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      redirect_back(fallback_location: root_path)
+      redirect_to "/items/#{@comment.item.id}"
     else
       redirect_back(fallback_location: root_path)
     end
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit( :id, :comment, :user_id, :item_id)
+    params.require(:comment).permit( :id, :comment).merge(user_id: current_user.id,item_id: current_user.id)
     # params.require(:item).permit(:name, :introduction, :price, :prefecture_code, :brand_id, :pref_id, :size_id, :item_condition_id, :postage_payer_id, :preparation_day_id, :postage_type_id, :category_id, :trading_status, item_imgs_attributes: [:url, :id]).merge(seller_id: current_user.id)
   end
 end
