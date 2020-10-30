79e59dc3f7ff516c1b09fe8f599ea3a5df9695d8
kobata2203 <kobata1569@icloud.com>
2020-10-31 03:45:13 +0900

    マイグレーション再構築2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_065036) do
+ActiveRecord::Schema.define(version: 2020_10_30_175936) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -97,10 +97,9 @@ ActiveRecord::Schema.define(version: 2020_10_22_065036) do
 
   create_table "item_imgs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "src", null: false
-    t.bigint "item_id", null: false
+    t.integer "item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_item_imgs_on_item_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -212,5 +211,4 @@ ActiveRecord::Schema.define(version: 2020_10_22_065036) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "item_imgs", "items"
 end
