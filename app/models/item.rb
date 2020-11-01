ee0616058d4805edaa4738d8c7202c7b7f31ee93
yu-ninji <ninjikamikami@icloud.com>
2020-11-01 21:00:54 +0900

    ok

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  has_many :item_categories, dependent: :destroy
-  has_many :categories, through: :item_categories
+  # has_many :item_categories, dependent: :destroy
+  # has_many :categories, through: :item_categories
   belongs_to :user
   # foreign_key: 'user_id'
   has_many :comments, dependent: :destroy
