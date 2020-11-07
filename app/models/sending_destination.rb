e2d2907efe7b04bdb269dcd0074dda1bd55d6cba
kobata2203 <kobata1569@icloud.com>
2020-11-07 20:34:38 +0900

    新規登録_都道府県コードの修正完了

--- app/models/sending_destination.rb
+++ app/models/sending_destination.rb
@@ -1,4 +1,6 @@
 class SendingDestination < ApplicationRecord
   belongs_to :user, optional: true
-  validates :first_name, :family_name , :first_name_kana , :family_name_kana , :post_code , :prefecture_code , :city , :house_number ,presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture_code
+  validates :first_name, :family_name , :first_name_kana , :family_name_kana , :post_code , :prefecture_code_id , :city , :house_number ,presence: true
 end
