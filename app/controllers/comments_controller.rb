326658f06a2f705243b1399ba1b0175d18230342
kobata2203 <kobata1569@icloud.com>
2020-11-19 15:42:07 +0900

    コメント機能の修正完了

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit( :id, :comment).merge(user_id: current_user.id,item_id: current_user.id)
+    params.require(:comment).permit( :id, :comment).merge(user_id: current_user.id,item_id: params[:item_id])
     # params.require(:item).permit(:name, :introduction, :price, :prefecture_code, :brand_id, :pref_id, :size_id, :item_condition_id, :postage_payer_id, :preparation_day_id, :postage_type_id, :category_id, :trading_status, item_imgs_attributes: [:url, :id]).merge(seller_id: current_user.id)
   end
 end
